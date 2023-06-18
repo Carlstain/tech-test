2435fdc45d9d2720939dc873d616652185e3f398
Mahadi Eric <rickyblackwall@gmail.com>
2023-06-18 04:37:26 +0200

    Add review view

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,7 @@
+from rest_framework.decorators import action
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet
 
 from movies.models import Movie
@@ -12,3 +16,17 @@ class MovieViewSet(ModelViewSet):
         if self.action == "list":
             return MovieSerializer
         return super().get_serializer_class()
+
+    @action(detail=True, methods=["post"], url_path="review")
+    def add_review(self, request, pk):
+        try:
+            Movie.objects.get(id=pk)
+        except Movie.DoesNotExist:
+            raise NotFound(f"Movie with id {pk} does not exist")
+
+        request.data["movie"] = pk
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
